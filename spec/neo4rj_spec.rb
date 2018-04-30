41552eb8c370a370871d339823834cc261afde52
Richard Julian <rjulian57@gmail.com>
2018-04-29 21:40:40 -0700

    Remove extraneous tests

--- spec/neo4rj_spec.rb
+++ spec/neo4rj_spec.rb
@@ -1,20 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../neo4rj.rb'
+require './lib/neo4rj'
 
 RSpec.describe Neo4rj do
-  context 'it creates a valid http listener' do
-    it 'has a port exposed on 7474' do
-    end
-    it 'requires the proper headers for http comms' do
-    end
-    it 'returns the same response no matter the query passed' do
-    end
-  end
-  context 'it creates a valid bolt protocol listener' do
-    it 'has a port explosed on 7658' do
-    end
-  end
   context 'it logs all connection information' do
     it 'creates a log file on start' do
       neo4rj = Neo4rj.new
@@ -23,13 +11,5 @@ RSpec.describe Neo4rj do
       expect(logger.progname).to eq('neo4rj')
       neo4rj = nil
     end
-    it 'logs IP address for a connection' do
-    end
-    it 'logs credentials used for a connection' do
-    end
-    it 'logs payloads of queries' do
-    end
-  end
-  it 'allows access with any bad credentials pairs' do
   end
 end
