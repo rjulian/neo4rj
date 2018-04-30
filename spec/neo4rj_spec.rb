3388557e98e2a5bcca7515104526d4d7f8f9d5d4
Richard Julian <rjulian57@gmail.com>
2018-04-29 22:07:43 -0700

    Cleaned up tests a bit

--- spec/neo4rj_spec.rb
+++ spec/neo4rj_spec.rb
@@ -3,13 +3,15 @@
 require './lib/neo4rj'
 
 RSpec.describe Neo4rj do
-  context 'it logs all connection information' do
-    it 'creates a log file on start' do
-      neo4rj = Neo4rj.new
-      logger = neo4rj.instance_variable_get(:@logger)
-      expect(logger.level).to eq(0)
-      expect(logger.progname).to eq('neo4rj')
-      neo4rj = nil
-    end
+  before do
+    @neo4rj = Neo4rj.new
   end
+
+
+  it 'creates a log file on start' do
+    logger = @neo4rj.instance_variable_get(:@logger)
+    expect(logger.level).to eq(0)
+    expect(logger.progname).to eq('neo4rj')
+  end
+
 end
