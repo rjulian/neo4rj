d487b899c0c1415de5ce952b082f5a42835e402c
Richard Julian <rjulian57@gmail.com>
2018-04-30 21:02:37 -0700

    More comprehensive tests of functionality

--- spec/neo4rj_spec.rb
+++ spec/neo4rj_spec.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
-
+require 'net/http'
 require './lib/neo4rj'
 
 RSpec.describe Neo4rj do
   before do
     @neo4rj = Neo4rj.new
+    Thread.new do
+      @neo4rj.start_http_service
+    end
   end
-
 
   it 'creates a log file on start' do
     logger = @neo4rj.instance_variable_get(:@logger)
@@ -14,4 +16,17 @@ RSpec.describe Neo4rj do
     expect(logger.progname).to eq('neo4rj')
   end
 
+  it 'has a delivery payload' do
+    expect(@neo4rj.payload).not_to be_nil
+  end
+
+  it 'returns the expected payload' do
+    uri = URI('http://localhost:7474')
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Get.new uri
+      response = http.request request
+      expect(response.body.include?('management')).to eq(true)
+      expect(response.code).to eq('200')
+    end
+  end
 end
